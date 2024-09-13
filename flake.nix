a536296058533c8cd8e62ae7379226709c99e56a
luanmtruong <luanmtruong@kobiton.com>
2024-09-13 13:18:43 +0700

    Update

--- flake.nix
+++ flake.nix
@@ -1,39 +1,54 @@
 {
-  description = "My workshop template";
+  description = "My slides";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    skm.url = "github:sagikazarmark/nur-packages";
-    skm.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv.url = "github:cachix/devenv";
+    mynur.url = "github:sagikazarmark/nur-packages";
+    mynur.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, skm, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
 
-          overlays = [
-            (final: prev: {
-              quarto = skm.packages.${system}.quarto;
-              decktape = skm.packages.${system}.decktape;
-            })
-          ];
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            git
-            gnumake
-            quarto
-            decktape
-          ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-          shellHook = ''
-            echo "quarto $(quarto --version)"
-          '';
+      perSystem = { config, self', inputs', pkgs, system, ... }: rec {
+        devenv.shells = {
+          default = {
+            packages = with pkgs; [
+              quarto
+              netlify-cli
+              d2
+
+              # python310Packages.jupyter
+              # python310Packages.matplotlib
+              # python310Packages.plotly
+            ] ++ [
+              inputs'.mynur.packages.decktape
+            ];
+
+            scripts = {
+              versions.exec = ''
+                echo "quarto $(quarto --version)"
+                netlify --version
+              '';
+            };
+
+            enterShell = ''
+              versions
+            '';
+
+            # https://github.com/cachix/devenv/issues/528#issuecomment-1556108767
+            containers = pkgs.lib.mkForce { };
+          };
+
+          ci = devenv.shells.default;
         };
-      });
+      };
+    };
 }
